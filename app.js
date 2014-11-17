0d8024d7faa144f1d2ad4c65d6237b761af7025a
ksaito256 <k.saito.256@gmail.com>
2014-11-17 09:29:06 +0000

    sample1

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-var express = require('express')
-var app = express()
+
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+    var name = req.param("name");
+    res.render('hello.ejs', {
+		title: "タイトル",
+		content: "コンテンツ",
+		name: name
+	});
+	
+    res.end();
 })
 
-var server = app.listen(3000, function () {
- console.log("app")
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
+var server = app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log('Example app listening at http://%s:%s', host, port)
+    console.log(__dirname)
+    
 })
 
 
